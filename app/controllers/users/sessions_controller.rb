0f7afc1cc2b74f345671577a44703ea7708df3ac
megumi <ooo.megumi9@gmail.com>
2023-12-26 17:27:02 +0900

    ゲストログインのメッセージ修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,12 +2,12 @@ class Users::SessionsController < Devise::SessionsController
   def general_guest_sign_in
     user = User.general_guest
     sign_in user
-    redirect_to new_user_session_path, notice: 'ゲストユーザー(一般)としてログインしました。'
+    redirect_to root_path, notice: 'ゲストユーザー(一般)としてログインしました。'
   end
 
   def admin_guest_sign_in
     user = User.admin_guest
     sign_in user
-    redirect_to new_user_session_path, notice: 'ゲストユーザー(管理者)としてログインしました。'
+    redirect_to root_path, notice: 'ゲストユーザー(管理者)としてログインしました。'
   end
 end
